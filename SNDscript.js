57cd0bfded0df5b2469d43dddf8be6d985e23a2b
FrogiInHoodie <114205312+FrogiInHoodie@users.noreply.github.com>
2022-10-03 14:03:43 +0800

    Add files via upload

---
+++ SNDscript.js
@@ -0,0 +1,11 @@
+const ques = document.getElementById("question")
+const b = document.querySelector("button.button--no")
+b.addEventListener("mouseover", moveHover)
+
+function moveHover() {
+    const i=Math.floor(Math.random()*500)+1;
+    const j=Math.floor(Math.random()*500)+1;
+
+    b.style.right=i+"px"
+    b.style.top=j+"px"
+}
